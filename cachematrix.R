6741944b035c10905fe7489f9b9e9c385f14bfff
Hu, Danielle <danielle.hu@elekta.com>
2015-11-22 13:58:16 -0800

    Add Comments
    Add Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Below are two functions that are used to create a special object that stores a numeric vector 
+#and cache's its mean.
 
-## Write a short comment describing this function
-
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -19,10 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function 
+# The following function returns the inverse of the matrix.
+# It checks if the inverse exists, if yes, then get it from cached data
+# otherwise, create inverse metrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
   if(!is.null(inv)) {
       message("getting cached data")
